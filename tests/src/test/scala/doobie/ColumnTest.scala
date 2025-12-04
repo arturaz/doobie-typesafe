a936c60a09f7934a3eda0ebadf5f0c419b44d9cc
Artūras Šlajus <x11@arturaz.net>
2025-12-04 19:07:46 +0200

    `Column.imap` test

--- tests/src/test/scala/doobie/ColumnTest.scala
+++ tests/src/test/scala/doobie/ColumnTest.scala
@@ -1,9 +1,16 @@
 package doobie
 
 import doobie.implicits.*
+import doobie.util.{DBFixture, Helpers}
 import munit.*
 
-class ColumnTest extends CatsEffectSuite {
+class ColumnTest extends CatsEffectSuite with DBFixture with Helpers {
+  private val withTable = db.mapAsync { xa =>
+    val create =
+      sql"create table $Person (${Person.nameCol} text not null, ${Person.ageCol} int not null)".update.run
+    create.transact(xa).void.map(_ => xa).unsafeToFuture()
+  }
+
   test("instantiate from custom Meta[A]") {
     class Foo(val a: Int)
     given Meta[Foo] = Meta[Int].timap(a => new Foo(a))(_.a)
@@ -29,5 +36,20 @@ class ColumnTest extends CatsEffectSuite {
     val c = Column[Option[Int]]("foo")
     assertEquals((c === None).fragment.toString, sql"foo IS NULL".toString)
     assertEquals((c === Some(3)).fragment.toString, sql"foo = ?".toString)
+  }
+
+  withTable.test("imap") { xa =>
+    val ageAsString = Person.ageCol.imap(_.age.toString)(s => Age(s.toInt))
+
+    val table = Person
+    val columns = Columns((Person.ageCol, ageAsString))
+    val sql = for {
+      _ <- table.insertInto(person ==> Person("Alice", Age(42))).update.run
+      _ <- table.updateTable(ageAsString --> "100").update.run
+      result <- sql"select $columns from $table".queryOf(columns).unique
+    } yield result
+
+    val actual = sql.transact(xa)
+    actual.assertEquals((Age(100), "100"))
   }
 }
